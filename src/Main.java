654e6e04888b9ea0d4627b404fb687ab2844973f
StephenDevs <stephennedd1@gmail.com>
2022-09-16 13:39:51 -0400

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,35 @@
+import javax.swing.*;
+import java.text.DecimalFormat;
+
+public class Main {
+
+    // this is a little program that calculates the price you pay when splitting an electric bill
+    // it takes the total bill and the usage in kilowatt and calculates the price each person pays
+    public static void main(String[] args) {
+
+        // create boolean to check if user wants to continue
+        boolean running = true;
+
+        // loop to keep program running
+        while (running) {
+
+            // create scanner to get user input and show in window
+            double billTotalInFlorin = Double.parseDouble(JOptionPane.showInputDialog("Enter the total bill in florin: "));
+            double myMeterInKwh = Double.parseDouble(JOptionPane.showInputDialog("Enter your usage in kWh: "));
+            double totalMeterKwh = Double.parseDouble(JOptionPane.showInputDialog("Enter the total meter reading in kWh: "));
+            double myPercentage = (myMeterInKwh / totalMeterKwh) * 100;
+
+            DecimalFormat df = new DecimalFormat("#.##");
+            double myBill = (billTotalInFlorin / 100) * myPercentage;
+
+            // show the result
+            JOptionPane.showMessageDialog(null, "Your bill is: " + df.format(myBill) + " florin");
+
+            // ask user if they want to calculate again
+            int answer = JOptionPane.showConfirmDialog(null, "Do you want to calculate another bill?");
+            if (answer == JOptionPane.NO_OPTION) {
+                running = false;
+            }
+        }
+    }
+}
